804bdd88779ef696a7f277ecaba23856a8d441a0
Daniil <daniil.bolgov@simbirsoft.com>
2020-02-28 23:09:06 +0400

    Initial commit

---
+++ src/main/kotlin/ru/samgtu/erp/ErpApplication.kt
@@ -0,0 +1,11 @@
+package ru.samgtu.erp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ErpApplication
+
+fun main(args: Array<String>) {
+	runApplication<ErpApplication>(*args)
+}
---
+++ src/test/kotlin/ru/samgtu/erp/ErpApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.samgtu.erp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
