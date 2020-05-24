f127536681afffdec83d75c9fa238555c3a68045
Trikcter <Trikcter>
2020-05-24 22:47:39 +0400

    ERP-007 Добавил графики, поправил ошибки

--- src/main/kotlin/ru/samgtu/erp/repository/BalanceRegistryRepository.kt
+++ src/main/kotlin/ru/samgtu/erp/repository/BalanceRegistryRepository.kt
@@ -2,8 +2,10 @@ package ru.samgtu.erp.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import ru.samgtu.erp.model.Balance
 import ru.samgtu.erp.model.BalanceRegistry
 
 @Repository
 interface BalanceRegistryRepository : JpaRepository<BalanceRegistry, Long> {
+    fun findAllByBalance(balance: Balance): List<BalanceRegistry>
 }
